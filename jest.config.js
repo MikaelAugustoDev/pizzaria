b899406ff289edeb8e1dc608837157cc7129ea2b
Mikael Augusto <mikaelaugustodev@gmail.com>
2023-06-12 10:57:19 -0300

    Instalando e Configurando as dependencias

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable linebreak-style */
+// eslint-disable-next-line linebreak-style
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+// eslint-disable-next-line no-undef
+module.exports = {
+	preset: "ts-jest",
+	testEnvironment: "node",
+	detectOpenHandles: true,
+};
